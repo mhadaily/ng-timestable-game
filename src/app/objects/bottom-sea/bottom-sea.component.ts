018fd0b7bb5572f1141f154110916977c6b91023
Majid <mhadaily@gmail.com>
2017-03-28 11:02:48 +0800

    styling

--- src/app/objects/bottom-sea/bottom-sea.component.ts
+++ src/app/objects/bottom-sea/bottom-sea.component.ts
@@ -3,12 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bottom-sea',
   templateUrl: './bottom-sea.component.html',
-  styleUrls: ['./bottom-sea.component.scss']
+  styleUrls: ['./bottom-sea.component.scss'],
 })
+
 export class BottomSeaComponent implements OnInit {
   constructor() { }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() { }
 }
